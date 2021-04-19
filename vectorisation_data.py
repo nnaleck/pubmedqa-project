3a3f3014b7027e7707de337e5cc41354d91bef0b
yasmin-atm <yasmine@MacBook-Air-de-Yasmine.local>
2021-04-19 13:42:17 +0200

    Add tf_idf vectorization

--- vectorisation_data.py
+++ vectorisation_data.py
@@ -4,63 +4,111 @@ import pandas as pd
 import numpy as np
 import re
 
+from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
 
 
 
-def vectorisation(df,ind):
+def corpus_list(df,ind) :
     """
-    Vectorization without column preprocessing
-
     Parameters
     ----------
-    df : dataframe
+    df1 : dataframe
         preprocessed dataset.
-    ind : string : 'Question', 'Long_Answer', 'Contexts' or 'Full_Text' 
+    ind : string : 'Question', 'Long_Answer', 'Contexts' or 'Full_Text'
         the column to vectorize.
 
     Returns
     -------
-    Matrix : vectorized matrix without preprocessing.
+    corpus : string list
+           list of ind.
 
     """
-    columns = [] #List of terms
-    lines = [] #List of ind
-    
+    corpus = [] #List of ind
     for i in df.index :
-        #In case no full_text
+        #In case no full_text : no new line
         if type(df[ind][i]) == str :
-            line = df[ind][i] 
-        lines.append(re.sub(r"[^a-zA-Z0-9 ]"," ",line))
-        #Get the words for each ind 
-        word_list = re.sub(r"[^a-zA-Z0-9 ]"," ",line).split() 
-        for word in word_list :
-            if word not in columns :
-                columns.append(word)
-                
-                
-    #Creation of the vectorization matrix
-    mat = pd.DataFrame(index=(lines),columns=(columns))
+            line = str(df[ind][i]) 
+        #Add line to corpus list without special characters and capital letters 
+        corpus.append(re.sub(r"[^a-zA-Z0-9 ]"," ",line.lower()))
+    
+    return corpus
 
-    for i in columns :
-        for j in lines :
-            if (i+' ') in j :
-                mat[i][j] = 1
-            else :
-                mat[i][j] = 0
-                
-    return(mat)
+    
 
-
-
-def vectorisation_1(df,ind):
+def words_to_delete(corpus):
     """
-    Processing of the vectorization matrix
+    Preprocessing of the words in the corpus list
 
     Parameters
     ----------
-    df : dataframe
-        vectorized data without column preprocessing.
-    ind : string : 'Question', 'Long_Answer', 'Contexts' or 'Full_Text' 
+    corpus : string list
+        list of text to process.
+
+    Returns
+    -------
+    words_to_delete : string list
+        list of words to delete.
+
+    """
+   
+    #List of words with length <= 3
+    words_to_delete1 = []
+    for text in corpus :
+        word_list = text.split() 
+        for word in word_list :
+            word = str(word)
+            if (word not in words_to_delete1) :
+                if (len(word) <=3):
+                    words_to_delete1.append(word)
+    
+    # expr = re.compile("\W+",re.U)
+    words_dico = dict() #Dictionary (word: occurrence)
+    for text in corpus: #For each sentence of the corpus
+        
+        # text = str(text)
+        # text = expr.split(text)
+        text = text.split()
+        for word in set(text): #Recovering each new word
+            if word not in words_dico:
+                words_dico[word]=1
+            else: #For each word already listed: add 1
+                words_dico[word]=words_dico[word]+1
+                
+    maximum = np.quantile(list(words_dico.values()), 0.9)
+    minimum = np.min(list(words_dico.values()))
+    
+    
+    words_freq = list()
+    for key, val in words_dico.items():
+        words_freq.append( (key, val) )
+    words_freq.sort(key=lambda tup: tup[1] ,reverse=True)
+    
+    #List of too rare words
+    words_to_delete2 = [t[0] for t in words_freq if t[1]<=minimum]
+    
+    
+    #List of the most frequent words (with quantile at 0.9)
+    words_to_delete3 = [t[0] for t in words_freq[:int(maximum)]]
+    
+    
+     
+    #Merge lists
+    words_to_delete = words_to_delete1 + words_to_delete2 + words_to_delete3
+    return words_to_delete
+
+  
+    
+    
+def vectorization(corpus,words_to_delete,ind) : 
+    """
+
+    Parameters
+    ----------
+    corpus : string list
+        list of text to process.
+    words_to_delete : string list
+        list of words to delete.
+    ind : string : 'Question', 'Long_Answer', 'Contexts' or 'Full_Text'
         the column to vectorize.
 
     Returns
@@ -68,53 +116,53 @@ def vectorisation_1(df,ind):
     None.
 
     """
-    #Remove the words <= 3
-    for i in df.columns :
-        if len(i)<=3 :
-            df.pop(i)
-      
-    #List of number of occurs for each word.       
-    list_occ = []
-    for j in df.iloc[:,1:] :
-        list_occ.append((df[j].sum()))
     
-    val_max = np.quantile(list_occ, 0.9)
-    val_min = np.min(list_occ)
+    #Stop_words : words to deleete
+    vectorizer = CountVectorizer(stop_words=words_to_delete,
+                                binary=True)
     
-    #List of indexes of max and min values
-    index_val_min = [indice for indice, valeur in enumerate(list_occ) if valeur==val_min]
-    index_val_max = [indice for indice, valeur in enumerate(list_occ) if valeur>=val_max]
+    X = vectorizer.fit_transform(corpus)
     
-    #List of indexes of columns to delete
-    list_del = sorted(index_val_max + index_val_min)
+    tokens = vectorizer.get_feature_names()
     
-    df = df.drop(df.columns[list_del], axis=1)
+    df_vect = pd.DataFrame(data = X.toarray(),
+                                index=(corpus),
+                                columns = tokens)
+    df_vect.to_csv(ind+'.csv', index=True) 
     
-    #Save the matrix in a csv file
-    df.to_csv( ind+'.csv', index=True)  
+    #vectorization with tf-idf
+    tfidfvectorizer = TfidfVectorizer(stop_words=words_to_delete,
+                                      use_idf=True,
+                                      smooth_idf=True)
     
+    Y = tfidfvectorizer.fit_transform(corpus)
     
-#Loading the dataset
+    tfidf_tokens = tfidfvectorizer.get_feature_names()
+    
+    df_tfidfvect = pd.DataFrame(data = Y.toarray(),
+                                index=(corpus),
+                                columns = tfidf_tokens)
+    
+    df_tfidfvect.to_csv(ind+'_tfidf.csv', index=True) 
+    
 df = pd.read_csv('ft.csv')
 
 #Vectorization of questions
-df1 = vectorisation(df, 'Question')
-vectorisation_1(df1, 'Question')
-
+corpus1 = corpus_list(df,'Question')
+words_to_delete1 = words_to_delete(corpus1)
+vectorization(corpus1,words_to_delete1,'Question') 
 
 #Vectorization of answers
-df2 = vectorisation(df, 'Long_Answer')
-df2 = df2.set_index(df['Document_ID'],append=True)
-vectorisation_1(df2, 'Long_Answer')
-
+corpus2 = corpus_list(df,'Long_Answer')
+words_to_delete2 = words_to_delete(corpus2)
+vectorization(corpus2,words_to_delete2,'Long_Answer') 
 
 #Vectorization of contexts
-df3 = vectorisation(df, 'Contexts')
-df3 = df3.set_index(df['Document_ID'],append=True)
-vectorisation_1(df3, 'Contexts')
-
+corpus3 = corpus_list(df,'Contexts')
+words_to_delete3 = words_to_delete(corpus3)
+vectorization(corpus3,words_to_delete3,'Contexts') 
 
 #Vectorization of full_text
-df4 = vectorisation(df, 'Full_Text')
-df4 = df4.set_index(df['Document_ID'],append=True)
-vectorisation_1(df4, 'Full_Text')
+corpus4 = corpus_list(df,'Full_Text')
+words_to_delete4 = words_to_delete(corpus4)
+vectorization(corpus4,words_to_delete4,'Full_Text') 
